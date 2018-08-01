a550122784e74c4735b7184937c1fc62984c4579
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-01 10:12:18 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=3,m=-3;
+		int a=n+m;
+		if(a%2==0)
+		System.out.print("even");
+		else
+		System.out.print("odd");
+	}
+}
